ab6a909fd14ca9be25c53b059a16a3b0cbd41b54
Lei Huang <leihuang6669@gmail.com>
2020-06-03 04:20:53 -0500

    update armada-cargo-test

--- test/cargo.go
+++ test/cargo.go
@@ -3,13 +3,14 @@ package main
 import (
   "github.com/armadanet/cargo"
   "log"
+  "os"
 )
 
 func main() {
-  r1 := &cargo.Request{
-    ReqType: 1,
-    Name: "test.txt",
-  }
+  // r1 := &cargo.Request{
+  //   ReqType: 1,
+  //   Name: "test.txt",
+  // }
   r2 := &cargo.Request{
     ReqType: 2,
     Name: "test.txt",
@@ -20,10 +21,10 @@ func main() {
     Name: "test.txt",
   }
   requester := cargo.NewRequester()
-  resp1 := requester.SendRequest(r1)
-  log.Println(resp1.Status)
-  s1 := string(resp1.Data)
-  log.Println(s1)
+  // resp1 := requester.SendRequest(r1)
+  // log.Println(resp1.Status)
+  // s1 := string(resp1.Data)
+  // log.Println(s1)
 
   resp2 := requester.SendRequest(r2)
   log.Println(resp2.Status)
@@ -34,4 +35,5 @@ func main() {
   log.Println(resp3.Status)
   s3 := string(resp3.Data)
   log.Println(s3)
+  os.Exit(0)
 }
---
+++ test/go.mod
@@ -0,0 +1,9 @@
+module github.com/armadanet/cargo/test
+
+go 1.13
+
+require (
+	github.com/armadanet/cargo v0.0.0-20200529125006-1504a548a341 // indirect
+	github.com/armadanet/comms v0.0.0-20200528090635-3089ce4375d7 // indirect
+	github.com/gorilla/mux v1.7.4 // indirect
+)
